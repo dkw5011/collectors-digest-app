1cc426f69d98ae66c01cece20b18ffb5f979114b
dkw5011 <dkw5011@gmail.com>
2020-05-09 11:56:26 -0400

    user controller edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,12 @@ class UsersController < ApplicationController
     end
 
     post '/login' do
-        @user = User.find_by(email: params[:email])
+        user = User.find_by(email: params[:email])
 
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
 
-            redirect '/users/#{@user.id}'
+            redirect '/users/#{user.id}'
         else
             redirect '/login'
         end
