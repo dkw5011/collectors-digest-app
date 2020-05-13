424093616ec9075e17cdc68b35f5860942186c1f
dkw5011 <dkw5011@gmail.com>
2020-05-13 00:08:09 -0400

    each collectable show page

--- app/controllers/collectables_controller.rb
+++ app/controllers/collectables_controller.rb
@@ -1,6 +1,12 @@
 class CollectablesController < ApplicationController
     get '/collectables' do
         @collectables = Collectable.all
-        erb :'/collectables/index'
+        erb :"/collectables/index"
     end
+
+    get '/collectables/:id' do
+        @collectable = Collectable.find(params[:id])
+        erb :"/collectables/show"
+    end
+
 end
