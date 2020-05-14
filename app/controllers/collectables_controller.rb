a378b756337b6a77a857972c7807a94f44dd4461
dkw5011 <dkw5011@gmail.com>
2020-05-13 21:06:09 -0400

    create new collectable route

--- app/controllers/collectables_controller.rb
+++ app/controllers/collectables_controller.rb
@@ -8,6 +8,11 @@ class CollectablesController < ApplicationController
         erb :"/collectables/new"
     end
 
+    post '/collectables' do
+        collectable = Collectable.create(name: params[:name], description: params[:description], image_url: params[:image_url], initial_cost: params[initial_cost], current_appraisel: params[:current_appraisel], user_id: current_user.id)
+        redirect "/collectables/#{collectable.id}"
+    end
+
     get '/collectables/:id' do
         @collectable = Collectable.find(params[:id])
         erb :"/collectables/show"
